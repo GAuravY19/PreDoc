bd646ab8dd2bca51bf977c362bb0fa40bd2c1a25
gaurav <gauravy1905@gmail.com>
2025-08-07 10:56:49 +0530

    Basic routes cleared

---
+++ predoc_app/routes.py
@@ -0,0 +1,26 @@
+from .app import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return "<h1> Hello World </h1>"
+
+@app.route('/start-home')
+def start_home():
+    return "<h1> Start Home </h1>"
+
+@app.route("/register")
+def register():
+    return "<h1> Register Page </h1>"
+
+@app.route("/login")
+def login():
+    return "<h1> Login Page </h1>"
+
+@app.route('/profile')
+def profile():
+    return "<h1> profile Page </h1>"
+
+@app.route("/logout")
+def logout():
+    return "<h1> Logout page </h1>"
