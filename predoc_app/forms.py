4bdcdaf25270b1376a654e6b3294eba6f8dd3482
gaurav <gauravy1905@gmail.com>
2025-08-25 09:45:03 +0530

    Phase 1 forms almost complete

--- predoc_app/forms.py
+++ predoc_app/forms.py
@@ -1,5 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, StringField, PasswordField, SubmitField, BooleanField, SelectField, DateField, TextAreaField, SelectMultipleField
+from wtforms import IntegerField, StringField, PasswordField, \
+    SubmitField, BooleanField, SelectField, DateField, \
+    TextAreaField, SelectMultipleField, widgets
 from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
 
 class RegistrationForm(FlaskForm):
@@ -51,18 +53,90 @@ class PersonalDetailsForm(FlaskForm):
 
 
 class LifestyleForm(FlaskForm):
-    pass
+    smoking_choice = SelectField('Smoking: ', choices=['Yes', 'No'],
+                                 validators=[DataRequired()])
+    alcohol_use = SelectField('Alcohol consumption: ', choices=['Yes', 'No'],
+                              validators=[DataRequired()])
+    substance_use = SelectField('Any Carcinogenic Substance intake: ', choices=['Yes', 'No'],
+                                validators=[DataRequired()])
+    exercise = SelectField('Exercise Frequency: ', choices=['Daily', 'Weekly', 'Rarely', 'No exercise routine'],
+                           validators=[DataRequired()])
+    sleep_hours = IntegerField('How much hours you sleep(Average): ',
+                               validators=[DataRequired()])
+    diet_pattern = SelectField('What type of diet pattern you follow: ', choices=['Veg', 'Non-Veg', 'Vegan'],
+                               validators=[DataRequired()])
 
 
 class MedicalHistoryForm(FlaskForm):
-    pass
+    diabetes_types = ['Type 1 Diabetes', 'Type 2 Diabetes', 'Gestational Diabetes',
+                      'Prediabetes', 'Monogenic Diabetes', 'Latent Autoimmune Diabetes in Adults (LADA) or Type 1.5 Diabetes',
+                      'Type 3c Diabetes', 'Steroid-Induced Diabetes', 'Cystic Fibrosis Diabetes',
+                      'Wolfram Syndrome', 'Type 5 Diabetes (Malnutrition-related Diabetes)', 'Secondary Diabetes', 'No']
+
+    hypertension_types = ['Secondary Hypertension', 'Resistant Hypertension', 'Isolated Systolic Hypertension',
+                          'Malignant Hypertension', 'White Coat Hypertension', 'Masked Hypertension', 'Hypertensive Emergency/Urgency', 'No']
+
+    cardiac_disease_types = ['Coronary Artery Disease', 'Heart Failure', 'Arrhythmias', 'Heart Valve Problems',
+                             'Cardiomyopathy', 'Congenital Heart Disease', 'Pericardial Disease', 'Rheumatic Heart Disease']
+
+    respiratory_types = ['Asthma', 'Chronic Obstructive Pulmonary Disease (COPD)', 'Bronchitis',
+                         'Pneumonia', 'Lung Cancer', 'Tuberculosis (TB)', 'Occupational respiratory diseases',
+                         'Pulmonary Fibrosis', 'Cystic Fibrosis', 'No']
+
+    diabetes = SelectField("Diabetes: ", choices=diabetes_types,
+                           validators=[DataRequired()])
+    hypertension = SelectField('Hypertension: ', choices=hypertension_types,
+                               validators=[DataRequired()])
+    cardiac_disease = SelectField('Any Cardiac disease: ', choices=cardiac_disease_types,
+                                  validators=[DataRequired()])
+    respiratory_disease = SelectField('Any Respiratory disease: ', choices=respiratory_types,
+                                      validators=[DataRequired()])
+    epilepsy = SelectField("Epilepsy: ", choices=['Yes', 'No'],
+                           validators=[DataRequired()])
+    mental_health_condition = TextAreaField('Any Mental Health conditions: ',
+                                            validators=[DataRequired()])
+    past_surgeries = SelectField('Any Past Surgeries: ', choices=['Yes', 'No'],
+                                 validators=[DataRequired()])
+    past_surgeries_type = TextAreaField('If yes, which surgery you had: ')
+
+    family_history = SelectMultipleField('Family History of Diseases: ', choices=[
+        ('diabetes', 'Diabetes'),
+        ('hypertension', 'Hypertension (High BP)'),
+        ('cardiac', 'Heart Disease'),
+        ('cancer', 'Cancer'),
+        ('mental', 'Mental Health Issues (Depression, Anxiety, etc.)'),
+        ('asthma', 'Asthma'),
+        ('none', 'None')
+    ], option_widget=widgets.CheckboxInput(),
+    widget=widgets.ListWidget(prefix_label=False))
 
 
 class AllergiesForm(FlaskForm):
-    pass
+    allergy_type_options = ['Food allergies', 'Drug (medication) allergies', 'Insect allergies',
+                            'Pet allergies', 'Mold allergies', 'Pollen allergies', 'Latex allergies',
+                            'Skin allergies', 'Allergic asthma', 'Eye allergies (allergic conjunctivitis)',
+                            'Anaphylaxis']
+
+    allergy_name_options = ['Milk allergy', 'Egg allergy', 'Peanut allergy', 'Tree nut allergy (e.g., walnuts, almonds, cashews)',
+                    'Shellfish allergy (e.g., shrimp, lobster, crab)', 'Fish allergy', 'Soy allergy', 'Wheat allergy',
+                    'Sesame allergy', 'Pollen allergy', 'House dust mite allergy', 'Pet dander allergy (cats and dogs)',
+                    'Mold allergy', 'Insect allergy', 'Drug allergies (e.g., penicillin)', 'Latex allergy',
+                    'Nickel allergy (contact dermatitis from jewelry and metals)', 'Perfume/cosmetic allergies']
+
+    allergy_status = SelectField('Any kind of allergy: ', choices=['Yes', 'No'],
+                                 validators=[DataRequired()])
+    allergy_type = SelectMultipleField('Type of allergy: ', choices=allergy_type_options)
+    allergy_name = SelectMultipleField("Allergy Name: ", choices=allergy_name_options)
+    allergy_reaction = TextAreaField('What is the allergy reaction: ')
 
 
 class CurrentMedicationForm(FlaskForm):
-    pass
+    medication = SelectField('Any medications undergoing: ', choices=['Yes', 'No'],
+                             validators=[DataRequired()])
+    drug_name = TextAreaField('Which medicines are you taking: ')
+    dosage = TextAreaField('What is the dosage of each Drug: ')
+    frequency = TextAreaField('What is the frequency of intake of each drug: ')
+    start_date = DateField("Start date: ", format='%Y-%m-%d')
+    end_date = DateField('When will the prescription end: ', format='%Y-%m-%d')
 
 
