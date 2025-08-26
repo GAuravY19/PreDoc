c214192171ca6303c22763655582854f3a9e11c6
gaurav <gauravy1905@gmail.com>
2025-08-26 11:28:33 +0530

    Postgresql connector code

--- predoc_app/routes.py
+++ predoc_app/routes.py
@@ -1,5 +1,19 @@
-from flask import render_template
-from .app import app
+from flask import render_template, flash, redirect, url_for
+from .forms import RegistrationForm, LoginForm
+from dotenv import load_dotenv
+import os
+import psycopg2
+from predoc_app import app, bcrypt
+
+load_dotenv()
+
+conn = psycopg2.connect(host = os.getenv("host"),
+                        dbname = os.getenv('db_name'),
+                        user = os.getenv('user'),
+                        password = os.getenv('password'),
+                        port = os.getenv('port'))
+
+curr = conn.cursor()
 
 @app.route('/')
 @app.route('/home')
@@ -10,13 +24,21 @@ def home():
 def start_home():
     return "<h1> Start Home </h1>"
 
-@app.route("/register")
+@app.route("/register", methods = ['GET', 'POST'])
 def register():
-    return "<h1> Register Page </h1>"
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        curr.execute(f'''INSERT INTO users (username, email, password_hash)
+                        VALUES (%s, %s, %s);''', (form.username.data, form.email.data, hashed_pw,))
+        conn.commit()
+        redirect(url_for('login'))
+    return render_template('register.html', form=form, css_file = 'register.css')
 
 @app.route("/login")
 def login():
-    return "<h1> Login Page </h1>"
+    form = LoginForm()
+    return render_template('login.html', form = form, css_file = 'login.css')
 
 @app.route('/profile')
 def profile():
