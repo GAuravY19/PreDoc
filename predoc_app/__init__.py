961c422de8e251ee7a886113c0b2c099f8937740
gaurav <gauravy1905@gmail.com>
2025-08-27 22:08:47 +0530

    App initializtion for login and logout systems

--- predoc_app/__init__.py
+++ predoc_app/__init__.py
@@ -1,7 +1,32 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv()
+
+host = os.getenv("host")
+dbname = os.getenv('db_name')
+user = os.getenv('user')
+password = os.getenv('password')
+port = os.getenv('port')
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '4015fab54c59c9f12e6096763ec201fc65219f0d9b2e0a317df0bb4714c00547'
+app.config['SECRET_KEY'] = os.getenv('secret_key')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{password}@{host}/{dbname}'
+db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+conn = psycopg2.connect(host = host,
+                        dbname = dbname,
+                        user = user,
+                        password = password,
+                        port = port)
+
+curr = conn.cursor()
