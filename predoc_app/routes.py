aaad22786c5d244de1187b62fd11fb145a12ad57
gaurav <gauravy1905@gmail.com>
2025-08-28 10:04:22 +0530

    New Routes for all basic details added

--- predoc_app/routes.py
+++ predoc_app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
-from .forms import RegistrationForm, LoginForm, PersonalDetailsForm
+from .forms import RegistrationForm, LoginForm, PersonalDetailsForm, \
+                    LifestyleForm, MedicalHistoryForm, AllergiesForm, CurrentMedicationForm
 from predoc_app import app, bcrypt, curr, conn, db
 from .model import User
 from flask_login import login_user, current_user, logout_user, login_required
@@ -61,27 +62,49 @@ def login():
     return render_template('login.html', form = form, css_file = 'login.css')
 
 
-@app.route('/personal_details')
+@app.route('/personal-details')
 @login_required
 def personal_details():
     form = PersonalDetailsForm()
 
+    if form.validate_on_submit():
+        return redirect(url_for('lifestyle_details'))
+
     return render_template('personal_details.html', form=form)
 
 
+@app.route('/lifestyle-details')
+@login_required
+def lifestyle_details():
+    form = LifestyleForm()
+
+    if form.validate_on_submit():
+        return redirect(url_for('medical-history'))
+
+    return render_template('lifestyle_details.html', form=form)
 
 
+@app.route('/medical-history')
+@login_required
+def medical_details():
+    form = MedicalHistoryForm()
+
+    return render_template('medical_history.html', form = form)
 
 
+@app.route('/allergy-details')
+@login_required
+def allergy_details():
+    form = AllergiesForm()
 
+    return render_template('allergy_details.html', form = form)
 
+@app.route('/current-medication-details')
+@login_required
+def current_medication_details():
+    form = CurrentMedicationForm()
 
-
-
-
-
-
-
+    return render_template('current_medication.html', form=form)
 
 
 
