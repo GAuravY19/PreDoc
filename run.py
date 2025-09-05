fed0e30343fefc7f4f4e1b8ff4a7ceaa2e67b31f
gaurav <gauravy1905@gmail.com>
2025-09-05 12:39:22 +0530

    Back to Debug mode

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from predoc_app.routes import app
 
 if __name__ == "__main__":
-    app.run(debug = False, host='0.0.0.0')
-    # app.run(debug=True)
+    # app.run(debug = False, host='0.0.0.0')
+    app.run(debug=True)
