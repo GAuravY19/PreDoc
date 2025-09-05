457b66a9f6087c091ee2bc7d01720329b7bc8258
gaurav <gauravy1905@gmail.com>
2025-09-05 12:57:42 +0530

    Removed connection db

--- predoc_app/__init__.py
+++ predoc_app/__init__.py
@@ -17,16 +17,10 @@ port = os.getenv('DB_PORT')
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.getenv('APP_SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_EXTERNAL')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://{user}:{password}@localhost:5432/{dbname}'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
-conn = psycopg2.connect(host = host,
-                        dbname = dbname,
-                        user = user,
-                        password = password,
-                        port = port)
 
-curr = conn.cursor()
