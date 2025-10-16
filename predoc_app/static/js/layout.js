7fa1e773dfdf75232fef1eb02ffa3b7024f6bc94
gaurav <gauravy1905@gmail.com>
2025-10-16 09:46:02 +0530

    Home page responsive done with hamburger menu

--- predoc_app/static/js/layout.js
+++ predoc_app/static/js/layout.js
@@ -1,12 +1,23 @@
-function togglemenu() {
-    const el = document.getElementsByClassName("nav-sections")[0];
 
-    const currentDisplay = window.getComputedStyle(el).display;
+document.addEventListener("DOMContentLoaded", function() {
+    const navbar = document.getElementById("navextend");
+    const ham = document.getElementById("hamburgerbtn");
+    const cross = document.getElementById("cross");
 
-    if (currentDisplay === 'none'){
-        el.style.display = 'flex';
-    } else {
-        el.style.display = 'none';
-    }
-}
+    ham.addEventListener("click", function(){
+        navbar.classList.toggle("show");
+    });
+
+    cross.addEventListener("click", function(){
+        navbar.classList.toggle("show");
+    });
+});
+
+// const navbar = document.getElementById("navextend");
+
+// function ShowingBurger() {
+//     alert("function activated")
+//     navbar.classList.toggle('show');
+// }
+
 
