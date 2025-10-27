ebacbfa168804f4fa198901e27d8a147e627cf02
gaurav <gauravy1905@gmail.com>
2025-10-27 17:40:49 +0530

    Minor change

--- run.py
+++ run.py
@@ -3,3 +3,5 @@ from predoc_app.routes import app
 if __name__ == "__main__":
     # app.run(debug = False, host='0.0.0.0')
     app.run(debug=True)
+    # app.run(host='0.0.0.0', port=5000, debug=True)
+
