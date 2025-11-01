56591911a319af0c5b3799a616db107644a0cb23
gaurav <gauravy1905@gmail.com>
2025-11-01 15:36:47 +0530

    Running file

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from predoc_app.routes import app
 
 if __name__ == "__main__":
-    app.run(debug = False, host='0.0.0.0')
-    # app.run(debug=True)
+    # app.run(debug = False, host='0.0.0.0')
+    app.run(debug=False)
     # app.run(host='0.0.0.0', port=5000, debug=True)
 
